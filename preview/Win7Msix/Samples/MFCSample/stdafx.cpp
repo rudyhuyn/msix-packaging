fb647162eafade1aba6dfde010eb1ecc98957b9a
Rudy Huyn <rudyhuyn@gmail.com>
2019-03-13 18:52:04 -0700

    remove sample

--- preview/Win7Msix/Samples/MFCSample/stdafx.cpp
+++
@@ -1,9 +0,0 @@
-
-// stdafx.cpp : source file that includes just the standard includes
-// MFCSample.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
